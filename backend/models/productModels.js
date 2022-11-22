1e35fe9f403758d656e68fb86d7fe90f6627e9dd
Aswathy N <aswathia.achu@gmail.com>
2022-11-22 09:43:27 +0530

    razorpay

--- backend/models/productModels.js
+++ backend/models/productModels.js
@@ -1,137 +1,149 @@
-// const mongoose = require('mongoose');
+
+
+// const mongoose = require("mongoose");
 
 // const prodctSchema = mongoose.Schema({
-//     name:{
-//         type:String,
-//         required:[true,"Please Enter Product Name"],
-//         trim:true
-//     },
-//     // description:{
-//     //     type:String,
-//     //     required:[true,"Please Enter Product Description"]
-//     // },
-//     price:{
-//         type:Number,
-//         required:[true,"Please Enter Product Price"],
-//         maxLength:[8,"Price cannot exceed 8 characters"]
-//     },
-//     discount:{
-//         type:Number,
-//         default:0
-//     },
-//     ratings:{
-//         type:Number,
-//         default:0
-//     },
-//     images:[
-//        {
-//         public_id :{
-//             type:String,
-//         required:true
-//         },
-//         url:{
-//             type:String,
-//         required:true
-//         }
-//        }
-//     ],
-//     category:{
-//         type:String,
-//         required:[true,"Please Enter Product Category"]
-
-//     },
-//     stock:{
-//         type:Number,
-//         required :[true,"Please Enter Product Stock"],
-//         maxLength:[4,"Stock cannot exceed 4 charactors"],
-//         default:1
-//     },
-//     numOfReviews:{
-//         type:Number,
-//         default:0
-//     },
-//     reviews:[
+//   name: {
+//     type: String,
+//     required: [true, "Please Enter Product Name"],
+//     trim: true,
+//   },
+//   // description:{
+//   //     type:String,
+//   //     required:[true,"Please Enter Product Description"]
+//   // },
+//   mrp: {
+//     type: Number,
+//     required: [true, "Please Enter Product Price"],
+//     maxLength: [8, "Price cannot exceed 8 characters"],
+//   },
+//   gst: {
+//     type: Number,
+//   },
+//   discount: {
+//     type: Number,
+//     // default:0
+//   },
+//   // ratings:{
+//   //     type:Number,
+//   //     default:0
+//   // },
+//   images: [
 //     {
-//         name:{
-//             type:String,
-//             required:true,
-//             default: null
-//         },
-//         avatar:{
-//             type:String,
-//             required:true,
-//             default: null
-//         },
-//         rating:{
-//             type:Number,
-//             required:true,
-//             default: null
-//         },
-//         comment:{
-//             type:String,
-//             required:true,
-//             default: null
-//         },
-//         user:{
-//             type:mongoose.Schema.ObjectId,
-//             ref:"User",
-//             required:true,
-//             default: null
-//         }
-//     }
-//     ],
-//    createdAt:{
-//         type:Date,
-//         default:Date.now
+//       public_id: {
+//         type: String,
+//         required: true,
+//       },
+//       url: {
+//         type: String,
+//         required: true,
+//       },
 //     },
-//     discountAmount:{
-//         type:Number,
-//         default: function() {
-//             return this.price * this.discount / 100
-//         }
+//   ],
+//   category: {
+//     type: String,
+//     required: [true, "Please Enter Product Category"],
+//   },
+//   qtyType: {
+//     type: String,
+//   },
+//   productType: {
+//     type: String,
+//   },
+//   stock: {
+//     type: Number,
+//     required: [true, "Please Enter Product Stock"],
+//     maxLength: [4, "Stock cannot exceed 4 charactors"],
+//     default: 1,
+//   },
+//   // numOfReviews:{
+//   //     type:Number,
+//   //     default:0
+//   // },
+//   // reviews:[
+//   // {
+//   //     name:{
+//   //         type:String,
+//   //         required:true,
+//   //         default: null
+//   //     },
+//   //     avatar:{
+//   //         type:String,
+//   //         required:true,
+//   //         default: null
+//   //     },
+//   //     rating:{
+//   //         type:Number,
+//   //         required:true,
+//   //         default: null
+//   //     },
+//   //     comment:{
+//   //         type:String,
+//   //         required:true,
+//   //         default: null
+//   //     },
+//   //     user:{
+//   //         type:mongoose.Schema.ObjectId,
+//   //         ref:"User",
+//   //         required:true,
+//   //         default: null
+//   //     }
+//   // }
+//   // ],
+//   createdAt: {
+//     type: Date,
+//     default: Date.now,
+//   },
+//   discountAmount: {
+//     type: Number,
+//     default: function () {
+//       return (this.mrp * (100 - this.discount)) / 100;
 //     },
-//     product_tax:{
-//         type:Number,
-//         required:[true,"Please Enter Tax "],
-//         default:18
+//   },
+//   // product_tax:{
+//   //     type:Number,
+//   //     required:[true,"Please Enter Tax "],
+//   //     default:18
+//   // },
+//   taxedAmount: {
+//     type: Number,
+//     default: function () {
+//       if (this.discount) return (this.discountAmount * (100 + this.gst)) / 100;
+//       else return (this.mrp * (100 + this.gst)) / 100;
 //     },
-//     taxedAmount:{
-//         type:Number,
-//         default: function() {
-//             return  this.price*this.product_tax/100
-//         }
-//     },
-//     actualPrice:{
-//         type:Number,
-//         default: function() {
-//             return this.price + this.taxedAmount
-//         }
-//     },
-    
-//     crossPriceVsActual:{
-//         type:Number,
-//         default:35
-//     },
-//     crossPrice:{
-//         type:Number,
-//         default: function() {
-//             return parseInt(this.actualPrice + ((this.actualPrice * this.crossPriceVsActual)/100))
-//         }
-//     },
-//     seller:{
-//         type:mongoose.Schema.ObjectId,
-//         ref: "User",
-//         required:true,
+//   },
+//   // actualPrice:{
+//   //     type:Number,
+//   //     default: function() {
+//   //         return this.price + this.taxedAmount
+//   //     }
+//   // },
 
-//     },
-//     isRemoved:{
-//         type:Boolean,
-//         default:false,
-//     }
+//   // crossPriceVsActual:{
+//   //     type:Number,
+//   //     default:35
+//   // },
+//   // crossPrice:{
+//   //     type:Number,
+//   //     default: function() {
+//   //         return parseInt(this.actualPrice + ((this.actualPrice * this.crossPriceVsActual)/100))
+//   //     }
+//   // },
+//   // seller:{
+//   //     type:mongoose.Schema.ObjectId,
+//   //     ref: "User",
+//   //     required:true,
 
-// })
+//   // },
+//   isRemoved: {
+//     type: Boolean,
+//     default: false,
+//   },
+// });
 
-// module.exports = mongoose.model("Product",prodctSchema);
+// module.exports = mongoose.model("Product", prodctSchema);
+
+
 
 const mongoose = require("mongoose");
 
@@ -233,11 +245,14 @@ const prodctSchema = mongoose.Schema({
       return (this.mrp * (100 - this.discount)) / 100;
     },
   },
-  // product_tax:{
-  //     type:Number,
-  //     required:[true,"Please Enter Tax "],
-  //     default:18
-  // },
+  product_tax: {
+    type: Number,
+    // required:[true,"Please Enter Tax "],
+    // default:18
+    default: function () {
+      return (this.discountAmount * this.gst) / 2;
+    },
+  },
   taxedAmount: {
     type: Number,
     default: function () {
